f82c5b236143aee5b8ad014ee372c64bcbd3d1cd
Anh_Le <lehoangmaianh126@gmail.com>
2022-03-03 00:03:31 -0500

    Adding 2 tests

--- app/src/test/java/com/cs2340/towerjackets/ExampleUnitTest.java
+++ app/src/test/java/com/cs2340/towerjackets/ExampleUnitTest.java
@@ -1,6 +1,9 @@
 package com.cs2340.towerjackets;
 
 import org.junit.Test;
+import com.cs2340.towerjackets.models.Player;
+import com.cs2340.towerjackets.models.game_config.Difficulty;
+import com.cs2340.towerjackets.models.game_config.GameConfiguration;
 
 import static org.junit.Assert.*;
 
@@ -14,4 +17,22 @@ public class ExampleUnitTest {
     public void additionIsCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    // Anh Le
+    @Test (expected = IllegalArgumentException.class)
+    public void nameIsNull() {
+        Difficulty diff = Difficulty.Easy;
+        // This test should pass since the following line should throw an IllegalArgumentException
+        Player newPlayer = new Player(null, new GameConfiguration(diff));
+    }
+
+    // Anh Le
+    @Test
+    public void testNameDifficultyCorrectlyStored() {
+        Difficulty diff = Difficulty.Normal;
+        Player newPlayer = new Player("Agatha", new GameConfiguration(diff));
+        assertEquals("Agatha", newPlayer.getName());
+        assertEquals(Difficulty.Normal, newPlayer.getConfig().getGameDifficulty());
+    }
+
 }
