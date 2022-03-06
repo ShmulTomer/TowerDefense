2f8824e8b3efec4d1fbd71da5978ff2bdd1a10a2
Helen <helenchen200311@gmail.com>
2022-03-05 22:02:19 -0500

    helen junit tests

--- app/src/test/java/com/cs2340/towerjackets/ExampleUnitTest.java
+++ app/src/test/java/com/cs2340/towerjackets/ExampleUnitTest.java
@@ -7,18 +7,22 @@ import com.cs2340.towerjackets.models.game_config.GameConfiguration;
 
 import static org.junit.Assert.*;
 
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ExampleUnitTest extends AppCompatActivity {
     @Test
     public void additionIsCorrect() {
         assertEquals(4, 2 + 2);
     }
 
-    // Anh Le - This tests whether our program would throw an exception when the input name is null
+    // M3 - Anh Le - This tests whether our program would throw an exception when the input name is null
     @Test (expected = IllegalArgumentException.class)
     public void nameIsNull() {
         Difficulty diff = Difficulty.Easy;
@@ -26,7 +30,7 @@ public class ExampleUnitTest {
         Player newPlayer = new Player(null, new GameConfiguration(diff));
     }
 
-    // Anh Le - This tests whether our program would correctly store the information input by the users
+    // M3 - Anh Le - This tests whether our program would correctly store the information input by the users
     // Tested on all difficulties. Tested with 3 different input name strings.
     @Test
     public void testNameDifficultyCorrectlyStored() {
@@ -44,6 +48,34 @@ public class ExampleUnitTest {
         Player newPlayer3 = new Player("Calvin", new GameConfiguration(diff3));
         assertEquals("Calvin", newPlayer3.getName());
         assertEquals(Difficulty.Hard, newPlayer3.getConfig().getGameDifficulty());
+    }
+
+    // M3 - Helen Chen - This tests whether the purchase of a tower in the shop is stored in the Player.
+    // Tested at easy and normal difficulty, tower one and tower two.
+    @Test
+    public void testTowerPurchaseStored() {
+        Player player1 = new Player("tower tester", new GameConfiguration(Difficulty.Easy));
+        Button buyT1 = findViewById(R.id.towerOneB);
+        buyT1.performClick();
+        assertEquals(40, player1.getMoney());
+        assertEquals(1, player1.getTowerOneInv());
+
+        Player player2 = new Player("tower tester", new GameConfiguration(Difficulty.Normal));
+        Button buyT2 = findViewById(R.id.towerTwoB);
+        buyT2.performClick();
+        assertEquals(0, player2.getMoney());
+        assertEquals(1, player2.getTowerTwoInv());
+    }
+
+    // M3 - Helen Chen - This tests whether a purchase with insufficient funds is processed or not.
+    // Tested at hard difficulty, tower three. Expects not purchasable.
+    @Test
+    public void testTowerPurchaseInsufficientFunds() {
+        Player player3 = new Player("tower tester", new GameConfiguration(Difficulty.Hard));
+        Button buyT3 = findViewById(R.id.towerThreeB);
+        buyT3.performClick();
+        assertEquals(50, player3.getMoney());
+        assertEquals(0, player3.getTowerThreeInv());
     }
 
 }
